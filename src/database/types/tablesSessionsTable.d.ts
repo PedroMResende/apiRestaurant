426873625f9888120b1d833ee9ab6e222a5aa54d
Pedro Resende <professorpedroresende@gmail.com>
2026-01-16 17:02:13 -0300

    create tableProducts tableTables tableSessionsTables

---
+++ src/database/types/tablesSessionsTable.d.ts
@@ -0,0 +1,6 @@
+type TableSessionsTable = { 
+    id: number, 
+    table_id: number,
+    opened_at: number, 
+    closed_at: number
+}; 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,16 @@
 import {Router} from "express"; 
 
 import { productsRoutes } from "./productsRoutes";
+import { tableRoutes } from "./tableRoutes"; 
+import {tableSessionsRoutes} from "./tablesSessionsRoutes"; 
 
 const routes = Router(); 
 
 routes.use("/products", productsRoutes);
+routes.use("/tables", tableRoutes); 
+routes.use("/tables-sessions", tableSessionsRoutes); 
+
+
 
 export { routes }; 
 
